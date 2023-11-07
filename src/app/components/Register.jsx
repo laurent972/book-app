2a3f58bbf362a72beab45dba0d10779015efee92
lrega81 <l.rega@dispofi.fr>
2023-11-07 16:30:32 +0100

    register

---
+++ src/app/components/Register.jsx
@@ -0,0 +1,92 @@
+'use client'
+import { useState } from "react"
+
+const Register = () =>{
+
+    const [firstName,setFirstName] =  useState('');
+    const [email,setEmail] = useState('');
+    const [password,setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+
+   
+    const register = (e) => {
+        e.preventDefault();
+     
+        
+      }
+     
+    const handleValidate = (e) =>{
+        e.preventDefault();
+        let isValid = true;
+        const emailRegex = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/
+
+        console.log(firstName,email,password, confirmPassword);
+
+    }  
+
+
+    return(
+        
+        <form className="w-full max-w-lg" onSubmit={handleValidate}>
+              <div className="flex flex-wrap -mx-3 mb-6">
+                <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                    <label className="uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-first-name">
+                        First Name
+                    </label>
+                    <input 
+                        className="w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" 
+                        id="grid-first-name" 
+                        type="text"
+                        placeholder="Prénom"
+                        onChange={(e) => setFirstName(e.target.value)}
+                        value={firstName}
+                    />
+                    <label className="uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-email">
+                        email
+                    </label>
+                    <input 
+                        className="w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" 
+                        id="grid-email" 
+                        type="email" 
+                        placeholder="email"
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                    <label className="uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-password">
+                        Mot de passe
+                    </label>
+                    <input 
+                        className="w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" 
+                        id="grid-password" 
+                        type="password" 
+                        placeholder="******"
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                    <label className="uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-confirm-pass">
+                        Confirmer mot de passe
+                    </label>
+                    <input 
+                        className="w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" 
+                        id="grid-confirm-pass" 
+                        type="password" 
+                        placeholder="******"
+                        onChange={(e) => setConfirmPassword(e.target.value)}
+                    />
+
+                    <button 
+                    type="submit" 
+                    className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+               
+                    >
+                        Submit
+                    </button>
+
+                </div>
+                
+              </div>
+        </form>
+
+
+    )
+}
+
+export default Register;
---
+++ src/app/register/page.jsx
@@ -0,0 +1,9 @@
+import Register from "../components/Register";
+
+const PageRegister = () =>{
+    return(
+            <Register />
+        )
+}
+
+export default PageRegister;
